863f5260947131dd5b9c3b8ec30ee8457a24939b
Khai Nguyen <khainguyen2004@gmail.com>
2025-05-12 09:25:36 -0700

    Revised

--- src/GoogleFormsFetcher.java
+++ src/GoogleFormsFetcher.java
@@ -14,56 +14,13 @@ public class GoogleFormsFetcher {
 
     public static void main(String[] args) {
 
-        List<Student> listStudent = new ArrayList<>();
+        HttpURLConnection connection = fetchApiResponse(WEB_APP_URL);
 
-        try {
-            HttpURLConnection conn = fetchApiResponse(WEB_APP_URL);
+        List<Student> listStudents = listStudents(connection);
 
-            assert conn != null;
-            if (conn.getResponseCode() != 200)
-            {
-                System.out.println("Error: Could not connect to API");
-            }
-            else {
-                StringBuilder resultJson = new StringBuilder();
+        List<Student> filterbyMajor = filteredStudents(listStudents, "Computer Science");
 
-                Scanner scanner = new Scanner(conn.getInputStream());
-
-                while(scanner.hasNext())
-                {
-                    resultJson.append(scanner.nextLine());
-                }
-
-                scanner.close();
-
-                conn.disconnect();
-
-                JSONParser parser = new JSONParser();
-
-                JSONArray jsonArray = (JSONArray) parser.parse(resultJson.toString());
-
-                for (Object obj : jsonArray)
-                {
-                    JSONObject info  = (JSONObject) obj;
-
-                    String name = (String) info.get("Name");
-                    long age = (long) info.get("Age");
-                    String major = (String) info.get("Major");
-                    String hobby = (String) info.get("Hobby");
-                    String interest = (String) info.get("Interest");
-
-                    Student student = new Student(name, age, major, hobby, interest);
-
-                    listStudent.add(student);
-                }
-
-                System.out.println(listStudent.get(2).getName());
-            }
-        }catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-
+        System.out.println("There are " + filterbyMajor.size() + " students with " + filterbyMajor.get(0).getMajor());
 
     }
     private static HttpURLConnection fetchApiResponse(String urlString) {
@@ -87,26 +44,71 @@ public class GoogleFormsFetcher {
         return null;
     }
 
-//        try
-//        {
-//            // Create a URL
-//            URL url = new URL(urlString);
-//
-//            // Attempt to open the connection using url
-//            HttpURLConnection connection =(HttpURLConnection)url.openConnection();
-//
-//            // Set the request method get, meaning just to get data from api
-//            connection.setRequestMethod("GET");
-//
-//            connection.connect();
-//
-//            return connection;
-//
-//
-//        }catch (IOException e)
-//        {
-//            e.printStackTrace();
-//        }
-//        return null;
+    private static List<Student> listStudents (HttpURLConnection conn) {
+        List<Student> listStudent = new ArrayList<>();
+
+        try {
+            conn = fetchApiResponse(WEB_APP_URL);
+
+            assert conn != null;
+            if (conn.getResponseCode() != 200) {
+                System.out.println("Error: Could not connect to API");
+            } else {
+                StringBuilder resultJson = new StringBuilder();
+
+                Scanner scanner = new Scanner(conn.getInputStream());
+
+                while (scanner.hasNext()) {
+                    resultJson.append(scanner.nextLine());
+                }
+
+                scanner.close();
+
+                conn.disconnect();
+
+                JSONParser parser = new JSONParser();
+
+                JSONArray jsonArray = (JSONArray) parser.parse(resultJson.toString());
+
+                for (Object obj : jsonArray) {
+                    JSONObject info = (JSONObject) obj;
+
+                    String name = (String) info.get("Name");
+                    long age = (long) info.get("Age");
+                    String major = (String) info.get("Major");
+                    String hobby = (String) info.get("Hobby");
+                    String interest = (String) info.get("Interest");
+
+                    Student student = new Student(name, age, major, hobby, interest);
+
+                    listStudent.add(student);
+                }
+                return listStudent;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    private static List<Student> filteredStudents (List<Student> list, String targetMajor)
+    {
+        List<Student> filterStudents = new ArrayList<>();
+
+        for (Student student : list)
+        {
+            if (student.getMajor().equalsIgnoreCase(targetMajor))
+            {
+                filterStudents.add(student);
+            }
+        }
+
+        return filterStudents;
+
+    }
+
+
+
 }
 
